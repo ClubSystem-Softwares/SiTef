d05f47c527e1d5c7575248457f6991475ae447a6
Matheus Lopes Santos <fale_com_lopez@hotmail.com>
2020-09-18 14:57:33 -0300

    Criação da base exception do package

---
+++ src/Exceptions/SiTefException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace CSWeb\SiTef\Exceptions;
+
+use Exception;
+
+/**
+ * SiTefException
+ *
+ * @author Matheus Lopes Santos <fale_com_lopez@hotmail.com>
+ * @version 1.0.0
+ * @package CSWeb\SiTef\Exceptions
+ */
+class SiTefException extends Exception
+{
+}
