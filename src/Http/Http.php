7f4eae0894094e3d23fcebf436b8c21da4752e8f
Matheus Lopes Santos <fale_com_lopez@hotmail.com>
2020-09-18 14:57:52 -0300

    Criação da classe Http e desenvolvimento do método que realiza as requisições ao backend

---
+++ src/Http/Http.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace CSWeb\SiTef\Http;
+
+use CSWeb\SiTef\AccessConfig;
+use CSWeb\SiTef\Exceptions\SiTefException;
+use GuzzleHttp\{
+    Client,
+    Exception\ClientException,
+    Exception\ServerException,
+    Psr7\Request
+};
+
+/**
+ * Http
+ *
+ * @author Matheus Lopes Santos <fale_com_lopez@hotmail.com>
+ * @version 1.0.0
+ * @package CSWeb\SiTef\Http
+ */
+class Http
+{
+    protected $config;
+
+    public function __construct(AccessConfig $config)
+    {
+        $this->config = $config;
+    }
+
+    public function request(string $verb, string $url, array $body, array $headers = [])
+    {
+        $client  = new Client(['base_url' => null]);
+        $request = new Request($verb, $url, $headers, $body);
+
+        try {
+            $response = $client->send($request);
+            $content  = $response->getBody()->getContents();
+
+            return json_decode($content, true);
+        } catch ( ClientException | ServerException $e ) {
+            throw new SiTefException($e->getMessage());
+        }
+    }
+}
